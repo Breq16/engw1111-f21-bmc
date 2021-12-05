de2a256b55aa3bb69af95410e0cb9508090faf95
Brooke Chalmers <breq@breq.dev>
2021-12-04 23:11:33 -0500

    Add sqrt code to artist piece

--- src/writing/Artist.tsx
+++ src/writing/Artist.tsx
@@ -75,6 +75,27 @@ function Expression({ expanded }: { expanded: boolean }) {
         intuition through the trickiest parts of the proof. And without it, the
         wall would collapse the moment someone leaned on it.
       </Pg>
+      <div className="mx-auto w-max overflow-x-auto">
+        <code className="whitespace-pre">
+          {`
+float Q_rsqrt( float number )
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}`}
+        </code>
+      </div>
     </div>
   )
 }
